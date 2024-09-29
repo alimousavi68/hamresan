6f14d14059a2c61b09fe63de3a14e4cc0d8aed3c
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-09-29 17:54:52 +0330

    make defulat file structrs

---
+++ assets/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: همرسان
+Description: افزونه همرسان - انتشار خودکار پست ها در چندین سایت به صورت همزمان 
+Version: 1.0
+Author: Hasht Behesht
+Author URI: https://www.hasht.ir
+*/
+
+
+// Declare Const vraibleS
+define('COP_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
+define('COP_PLUGIN_URL', plugins_url('', __FILE__));
+
+// Includes Files
